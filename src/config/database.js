a48da9303c2cf881cd975b3a921fea4ae4e166e3
Javier Mtz <luisjaviermtzb@gmail.com>
2024-11-27 19:09:03 -0600

    update-api-configuracion

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize";
-import mysql2 from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,7 +6,6 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
   dialect: "mysql",
-  dialectModule: mysql2, // Especificar mysql2 directamente
 });
 
 export default sequelize;
